76faf5d392c7993b17a5c666de85abe8daab3b34
Akbarxoja Joraxojayev <akbarjoraxojayev7@gmail.com>
2025-08-04 15:15:26 +0500

    fully app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+   const [state, setstate] = useState([])
+   const [loading, setLoading] = useState(true);
+  
+   useEffect(()=> {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setstate(data))
+      .catch(error => console.error("Error fetching users:", error));
+   })
+  
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      <ul className="userList">
+        {state.map((user)=>{
+          return (
+            <li key={user.id}>
+              {user.name} - {user.email}
+            </li>
+          );
+        })}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
